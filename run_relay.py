a0c5980881a3b4fceb36385e7278cd6cb8baa5d2
forwil <forwil@foxmail.com>
2020-05-29 23:41:48 +0800

    move to utils

--- run_relay.py
+++ run_relay.py
@@ -1,44 +1,14 @@
-import tvm
-
-device = "x86"
-
-def get_model(path):
-    graph = open(path + ".json").read()
-    lib = tvm.module.load(path + ".tar")
-    params = bytearray(open(path + ".params", "rb").read())
-    return graph, lib, params
-
-def create_target(device, did = 1):
-    if device == "x86":
-        target = tvm.cpu(did)
-    elif device == "gpu":
-        target = tvm.gpu(did)
-    return target
-
-def speed(graph, lib, param, target):
-    import numpy as np
-    import tvm.contrib.graph_runtime as runtime
-    import json
-    graph_dict = json.loads(graph)
-    input_shape = graph_dict["attrs"]["shape"][1][0]
-    input_name = graph_dict["nodes"][0]["name"]
-    data_tvm = tvm.nd.array(np.random.uniform(size = input_shape).astype("float32"))
-    module = runtime.create(graph, lib, target)
-    module.set_input(input_name, data_tvm)
-    module.load_params(params)
-    ftimer = module.module.time_evaluator("run", target, number = 1, repeat = 10)
-    prof_res = np.array(ftimer().results) * 1000
-    return np.mean(prof_res)
+from utils import *
 
 if __name__ == "__main__":
     import argparse
+    import os
     parser = argparse.ArgumentParser(description = "run relay model")
     parser.add_argument("relay", help = "relay model path")
     parser.add_argument("-d", "--device", default="x86", choices=["gpu","x86"])
     arg = parser.parse_args() 
     graph, lib, params = get_model(arg.relay)
-    target = create_target(arg.device) 
-    time =speed(graph, lib, params, target)
-    import os
+    ctx = create_ctx(arg.device) 
+    time = speed(graph, lib, params, ctx)
     name = os.path.basename(arg.relay)
     print("%s, %.2f" % (name, time))
--- utils.py
+++ utils.py
@@ -1,12 +1,11 @@
 import tvm.relay as relay
 import tvm
 
-def get_onnx(path):
-    import onnx
-    on = onnx.load(open(path, "rb"))
-    name = on.graph.input[0].name
-    input_shape = [i.dim_value for i in  on.graph.input[0].type.tensor_type.shape.dim]  
-    return on, {name : input_shape}
+def get_model(path):
+    graph = open(path + ".json").read()
+    lib = tvm.module.load(path + ".tar")
+    params = bytearray(open(path + ".params", "rb").read())
+    return graph, lib, params
 
 def create_target(device):
     if device == "x86":
@@ -14,6 +13,35 @@ def create_target(device):
     elif device == "gpu":
         target = tvm.target.cuda()
     return target
+
+def create_ctx(device, did = 1):
+    if device == "x86":
+        ctx = tvm.cpu(did)
+    elif device == "gpu":
+        ctx = tvm.gpu(did)
+    return ctx
+
+def speed(graph, lib, params, ctx):
+    import numpy as np
+    import tvm.contrib.graph_runtime as runtime
+    import json
+    graph_dict = json.loads(graph)
+    input_shape = graph_dict["attrs"]["shape"][1][0]
+    input_name = graph_dict["nodes"][0]["name"]
+    data_tvm = tvm.nd.array(np.random.uniform(size = input_shape).astype("float32"))
+    module = runtime.create(graph, lib, ctx)
+    module.set_input(input_name, data_tvm)
+    module.load_params(params)
+    ftimer = module.module.time_evaluator("run", ctx, number = 1, repeat = 10)
+    prof_res = np.array(ftimer().results) * 1000
+    return np.mean(prof_res)
+
+def get_onnx(path):
+    import onnx
+    on = onnx.load(open(path, "rb"))
+    name = on.graph.input[0].name
+    input_shape = [i.dim_value for i in  on.graph.input[0].type.tensor_type.shape.dim]  
+    return on, {name : input_shape}
 
 def build_model(onnx_model, input_shape, target):
     model, relay_params = relay.frontend.from_onnx(onnx_model, input_shape)
