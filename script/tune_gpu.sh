23e8cc9c4750a7522ff672132304c36045eeaedc
forwil <forwil@foxmail.com>
2020-05-31 00:42:35 +0800

    update

--- script/tune_gpu.sh
+++ script/tune_gpu.sh
@@ -1,4 +1,4 @@
-for i in `cat list`
+for i in `cat list_8`
 do
     python tune.py models/$i.onnx -d gpu -t 1000 &
 done
--- tools/kill_python.sh
+++ tools/kill_python.sh
@@ -1,2 +1,2 @@
-ps -ef | grep yuf | grep python | grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | grep yuf | grep tune | grep -v grep | awk '{print $2}' | xargs kill -9
 
